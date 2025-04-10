8901d075c123d784097511da1ef24d5f1db8201b
Twormito <146174708+Twormito@users.noreply.github.com>
2025-04-10 17:45:58 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -14,17 +14,23 @@ def hello_world():
 key = Fernet.generate_key()
 f = Fernet(key)
 
-@app.route('/encrypt/<string:valeur>')
-def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+@app.route('/encrypt/<key>/<string:valeur>')
+def encryptage_cle_perso(key, valeur):
+    try:
+        f = Fernet(key.encode())  # Crée un objet Fernet avec la clé fournie
+        token = f.encrypt(valeur.encode())  # Chiffre la valeur
+        return f"Valeur encryptée : {token.decode()}"
+    except Exception as e:
+        return f"Erreur : {str(e)}"
 
-@app.route('/decrypt/<string:token>')
-def decryptage(token):
-    token_bytes = token.encode()  # Conversion str -> bytes
-    valeur = f.decrypt(token_bytes)  # Décryptage
-    return f"Valeur décryptée : {valeur.decode()}"  # Retourne la valeur en str
+@app.route('/decrypt/<key>/<token>')
+def decryptage_cle_perso(key, token):
+    try:
+        f = Fernet(key.encode())  # Crée un objet Fernet avec la clé fournie
+        valeur = f.decrypt(token.encode())  # Déchiffre le token
+        return f"Valeur décryptée : {valeur.decode()}"
+    except Exception as e:
+        return f"Erreur : {str(e)}"
   
 if __name__ == "__main__":
   app.run(debug=True)
